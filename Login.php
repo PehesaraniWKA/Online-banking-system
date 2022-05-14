584d58a13d200868670e5a35484771645abee902
Nisal-Narasinghe <narasinghenisal24@gmail.com>
2022-05-14 10:55:56 +0530

    Login page under creating

---
+++ Login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<link rel="stylesheet" type="text/css" href="styles/login.css">
+
+	<title>ABC Bank User Login</title>
+</head>
+<body>
+	<div class="container">
+		<form class="login-email">
+			<p class="login-text" style="font-size: 2rem; font-weight: 800;">Login in to your Account</p>
+			<div class="input-group">
+				<input type="email" placeholder="Email" required>
+			</div>
+			<div class="input-group">
+				<input type="password" placeholder="Password" required>
+			</div>
+			<div class="input-group">
+				<button class="btn">Login</button>
+			</div>
+			<p class="login-register-text">Not Registered Yet<br>Get the best online banking experience with ABC Bank<br><a href="register.php">Register Now</a></p>
+		</form>
+	</div>
+</body>
+</html>
