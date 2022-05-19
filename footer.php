dc532bdd834dd0f09a76aa0c8a1aa4ec5eab73df
Nisal-Narasinghe <narasinghenisal24@gmail.com>
2022-05-19 12:50:23 +0530

    added new style format to and footer style 50precent  edited

--- footer.php
+++ footer.php
@@ -1,71 +1,51 @@
 <!--Horizontal line-->
 <hr style="width:100%"> 
         <!--Footer-->
-        <section id="bottom">
+        <section class="bottom">
             <footer>
-                <table width="100%">
-                    <tr>
-                        <td><img src="" alt="QR code" width="500px" height="300px"></td>
-                        <td>
-                            <h3>Careers</h3>
-                        </td>
-                        <td>
-                            <h3>News & Evenets</h3>
-                        </td>
-                        <td>
-                            <h3>Get in touch</h3>
-                        </td>
-                        <td>
-                            <h3>Find us</h3>
-                        </td>
-                        <td></td>
-                        <td>
-                            <h3>Call us</h3>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Scan for mobile application</td>
-                        <td>
-                            <ul>
-                                <li>Internships</li>
-                                <li>Management Trainer</li>
-                            </ul>
-                        </td>
-                        <td>
-                            <ul>
-                                <li>Central Bank New policies</li>
-                                <li>"Husma" Charity Funds</li>
-                            </ul>
-                        </td>
-                        <td>
-                            <ul>
-                                <li>Address: No.90/A, Flower road, Colombo 7</li>
-                                <li>Emails: <a href="" id="footerlink">info@abcbank.lk</a></li>
-                                <li>Fax: <a href="" id="footerlink">+94658741235</a></li>
-                                <li>Call us: <a href="" id="footerlink">+94754123624</a></li>
-                            </ul>
-                        </td>
-                        <td class="column">
-                            <p><img src="images/img_snow.jpg" alt="Snow" style="width:100%"></p>
-                            <p><img src="images/img_forest.jpg" alt="Forest" style="width:100%"></p>
-                            <p><img src="images/img_mountains.jpg" alt="Mountains" style="width:100%"></p>
-                        </td>
-                        <td>
-                            <p></p>
-                        </td>
-                        <td class="srch-icon">
-                            <p><img src="images/Search icon.jpg" alt="Call icon" style="width:100%">1616</p>
-                        </td>
-                    </tr>
-                </table>
+                <div class="flex">
+                    <div class="inputBox">
+                        <img src="" alt="QR code" width="500px" height="300px"></br>
+                        <span>Scan for mobile application</span>
+                    </div>
+                    <div class="inputBox">	    
+                        <h3>Careers</h3>
+                        <span>Internships</span>
+                        <span>Management Trainer</span>
+                    </div>       
+                    <div class="inputBox">        
+                        <h3>News & Evenets</h3>
+                        <span>Central Bank New policies</span>
+                        <span>"Husma" Charity Funds</span>
+                    </div>  
+                    <div class="inputBox"> 
+                        <h3>Get in touch</h3>
+                        <span>Address: No.90/A, Flower road, Colombo 7</span>
+                        <span> Emails: <a href="" id="footerlink">info@abcbank.lk</a></span>
+                        <span> Fax: <a href="" id="footerlink">+94658741235</a></span>
+                        <span> Call us: <a href="" id="footerlink">+94754123624</a></span>
+                    </div>     
+                    <div class="inputBox">     
+                        <h3>Find us</h3>
+                        <p><img src="images/img_snow.jpg" alt="Snow" class="img"></p>
+                        <p><img src="images/img_forest.jpg" alt="Forest" class="img"></p>
+                        <p><img src="images/img_mountains.jpg" alt="Mountains" class="img"></p>
+                    </div>     
+                    <div class="inputBox">     
+                        <h3>Call us</h3>
+                        <p><img src="images/Search icon.jpg" alt="Call icon" class="img"></p>
+                        <span>1616</span>
+                    </div>
+                    </footer>
+        </section>
+           
                 <hr style="width:100%">
                 <center>
                     <p>2022 ABC Bannk PLC.ALL Rights Reserved|Customer Charter & Disclaimer|Solution by:NamsriSolution</p>
                 </center>
-            </footer>
-        </section>
+                <hr style="width:100%">
         <script src="js files/Home.js"></script>
     </div>
 </body>
-
+     
 </html>
