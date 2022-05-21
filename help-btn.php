a42bffd3b8ed4d4e6cc2a9934b8ae4092aac553d
IT21258480 <100502665+Pathumdissanayake@users.noreply.github.com>
2022-05-21 20:44:13 +0530

    Add files via upload

---
+++ help-btn.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>ABC Bank</title>
+    <link rel="stylesheet" href="styles/help-btn.css">
+    </head>
+    <body>
+        <div class="sticky">
+            <a href="inquaries.php">Help and support</a></div>
+        </div>
+    </body>
+</html>
