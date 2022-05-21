ff0a2b3b12689436be009408896c92c4133565fd
PehesaraniWKA <91754460+PehesaraniWKA@users.noreply.github.com>
2022-05-22 02:16:46 +0530

    removed bank name

--- slide show.php
+++ slide show.php
@@ -1,26 +1,21 @@
  <!--Slide show-->
  <div class="slideshow-container">
 
-<div class="Slides fade">
-  <img src="images/slideshow image 4.png" style="width:100%">
-  <div class="text">ABC Bank</div>
-</div>
+    <div class="Slides fade">
+      <img src="images/slideshow image 4.png" style="width:100%">
+    </div>
 
-<div class="Slides fade">
-  <img src="images/slideshow image 2.png" style="width:100%">
-  <div class="text">ABC Bank</div>
-</div>
+    <div class="Slides fade">
+      <img src="images/slideshow image 2.png" style="width:100%">
+    </div>
 
-<div class="Slides fade">
-  <img src="images/slideshow image 3.png" style="width:100%">
-  <div class="text">ABC Bank</div>
-</div>
+    <div class="Slides fade">
+      <img src="images/slideshow image 3.png" style="width:100%">
+    </div>
 
-<div class="Slides fade">
-    <img src="images/Job vacancies.png" style="width:100%">
-    <div class="text">ABC Bank</div>
-  
-</div>
+    <div class="Slides fade">
+        <img src="images/Job vacancies.png" style="width:100%">
+    </div>
 </div>
 <br>
 
