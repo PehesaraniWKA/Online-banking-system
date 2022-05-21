d888df9bcca913a55d6a6fd6a90faee1ac60cd3b
IT21258480 <100502665+Pathumdissanayake@users.noreply.github.com>
2022-05-21 20:16:48 +0530

    Update inquaries.php

--- inquaries.php
+++ inquaries.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -35,10 +39,11 @@
 
         <div id="mySidebar" class="sidebar">
             <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-                <a href="accounts.php">Accounts</a>
-                <a href="loan and leasing.php">Loan&Leasing</a>
-                <a href="cards.php">Cards</a>
-                <a href="Financial calculators.php">Financial Calculators</a>
+            <a href="account.php">Accounts</a>
+            <a href="#">Loan&Leasing</a>
+            <a href="#">E-banking</a>
+            <a href="#">Promotion</a>
+            <a href="cards.php">Cards</a>
             <br><br><br> <br><br><br> <br><br><br> <br><br><br> <br><br><br>
 
             <!--Calender-->
@@ -80,6 +85,7 @@
         
     <!--Nav bar-->
     <center>
+        <hr>
     <div class="helpNsup-nav">
         
         <a class="active" href="#inquaries">Inquaries</a>
