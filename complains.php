095736410c516ebb27dd95592ca8990648cd1075
IT21258480 <100502665+Pathumdissanayake@users.noreply.github.com>
2022-05-22 18:46:10 +0530

    Update complains.php

--- complains.php
+++ complains.php
@@ -72,6 +72,12 @@
             <a class="openbtn" onclick="openNav()">&#9776; Services</a>
             
             <a class="active" href="#helpandsupport">Help and Support</a>
+            <div class="search-container">
+                <form action="/action_page.php">
+                    <input type="text" placeholder="Search.." name="search">
+                    <button type="submit"><i class="fa fa-search"></i></button>
+                </form>
+            </div>
         </div>
 
     <!--Nav bar-->
@@ -129,9 +135,76 @@
 <p class="helpNsup-lastpara"><em> Email us: customerexperience@ABC.lk </p>
 <p class="helpNsup-lastpara"> write us: No:XX, Galle Road, Colombo XX, SriLanka. </em></p>
 
-    <?php include_once('footer.php');
-    ?>
-
-<script src="js files/helpNsup.js"></script>
+        <!--Horizontal line-->
+<hr style="width:100%"> 
+        <!--Footer-->
+        <section id="bottom">
+            <footer>
+                <table width="100%">
+                    <tr>
+                        <td><img src="" alt="QR code" width="500px" height="300px"></td>
+                        <td>
+                            <h3>Careers</h3>
+                        </td>
+                        <td>
+                            <h3>News & Evenets</h3>
+                        </td>
+                        <td>
+                            <h3>Get in touch</h3>
+                        </td>
+                        <td>
+                            <h3>Find us</h3>
+                        </td>
+                        <td></td>
+                        <td>
+                            <h3>Call us</h3>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Scan for mobile application</td>
+                        <td>
+                            <ul>
+                                <li>Internships</li>
+                                <li>Management Trainer</li>
+                            </ul>
+                        </td>
+                        <td>
+                            <ul>
+                                <li>Central Bank New policies</li>
+                                <li>"Husma" Charity Funds</li>
+                            </ul>
+                        </td>
+                        <td>
+                            <ul>
+                                <li>Address: No.90/A, Flower road, Colombo 7</li>
+                                <li>Emails: <a href="" id="footerlink">info@abcbank.lk</a></li>
+                                <li>Fax: <a href="" id="footerlink">+94658741235</a></li>
+                                <li>Call us: <a href="" id="footerlink">+94754123624</a></li>
+                            </ul>
+                        </td>
+                        <td class="column">
+                            <p><img src="images/img_snow.jpg" alt="Snow" style="width:100%"></p>
+                            <p><img src="images/img_forest.jpg" alt="Forest" style="width:100%"></p>
+                            <p><img src="images/img_mountains.jpg" alt="Mountains" style="width:100%"></p>
+                        </td>
+                        <td>
+                            <p></p>
+                        </td>
+                        <td class="srch-icon">
+                            <p><img src="images/Search icon.jpg" alt="Call icon" style="width:100%">1616</p>
+                        </td>
+                    </tr>
+                </table>
+                <hr style="width:100%">
+                <center>
+                    <p>2022 ABC Bannk PLC.ALL Rights Reserved|Customer Charter & Disclaimer|Solution by:NamsriSolution</p>
+                </center>
+            </footer>
+        </section>
+        <script src="js files/Home.js"></script>
+    </div>
+        
+    <script src="js files/helpNsup.js"></script>
 </body>
+
 </html>
