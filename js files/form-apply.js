d3708a0b8073b05002ecb1aeab2cfcae1432f949
IT21258480 <100502665+Pathumdissanayake@users.noreply.github.com>
2022-05-23 18:47:15 +0530

    Add files via upload

---
+++ js files/form-apply.js
@@ -0,0 +1,10 @@
+function enableButton(){
+    if(document.getElementById("checkbox").checked)
+    {
+        document.getElementById("submitBtn").disabled=false;
+    }
+    else{
+        document.getElementById("submitBtn").disabled=true;
+    }
+  
+  }
