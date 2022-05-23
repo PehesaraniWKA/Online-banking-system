ebef21a1d0ceeee5a9ebf81fe542f600f47b14d3
Nisal-Narasinghe <narasinghenisal24@gmail.com>
2022-05-24 02:09:28 +0530

    slide show error fixed width 100% and hight 500px

--- slide show.php
+++ slide show.php
@@ -3,19 +3,19 @@
  <div class="slideshow-container">
 
     <div class="Slides fade">
-      <img src="images/slideshow image 4.png" style="width:100%">
+      <img src="images/slideshow image 4.png" width="100%" height="500px" style="border-radius:20px">
     </div>
 
     <div class="Slides fade">
-      <img src="images/slideshow image 2.png" style="width:100%">
+      <img src="images/slideshow image 2.png" width="100%" height="500px" style="border-radius:20px">
     </div>
 
     <div class="Slides fade">
-      <img src="images/slideshow image 3.png" style="width:100%">
+      <img src="images/slideshow image 3.png" width="100%" height="500px" style="border-radius:20px">
     </div>
 
     <div class="Slides fade">
-        <img src="images/Job vacancies.png" style="width:100%">
+        <img src="images/Job vacancies.png" width="100%" height="500px" style="border-radius:20px">
     </div>
 </div>
 <br>
