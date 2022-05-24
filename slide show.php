b9ae00e5004a4b58b79d9b84abe5220ebcbba860
Nisal-Narasinghe <narasinghenisal24@gmail.com>
2022-05-24 10:09:52 +0530

    slide show

--- slide show.php
+++ slide show.php
@@ -3,19 +3,19 @@
  <div class="slideshow-container">
 
     <div class="Slides fade">
-      <img src="images/slideshow image 4.png" width="100%" height="500px" style="border-radius:20px">
+      <img src="images/slideshow image 4.png" width="100%" height="500px" style="border-radius:4px">
     </div>
 
     <div class="Slides fade">
-      <img src="images/slideshow image 2.png" width="100%" height="500px" style="border-radius:20px">
+      <img src="images/slideshow image 2.png" width="100%" height="500px" style="border-radius:4px">
     </div>
 
     <div class="Slides fade">
-      <img src="images/slideshow image 3.png" width="100%" height="500px" style="border-radius:20px">
+      <img src="images/slideshow image 3.png" width="100%" height="500px" style="border-radius:4px">
     </div>
 
     <div class="Slides fade">
-        <img src="images/Job vacancies.png" width="100%" height="500px" style="border-radius:20px">
+        <img src="images/Job vacancies.png" width="100%" height="500px" style="border-radius:4px">
     </div>
 </div>
 <br>
